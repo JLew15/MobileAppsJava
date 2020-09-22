61f509b433d93d914c6480c5f891a89c97c62f43
JLew15 <zombears@icloud.com>
2020-09-22 10:17:28 -0600

    Chapter4

---
+++ chapter4/src/jaiden/chapter4/lesson2/Lesson2exe.java
@@ -0,0 +1,40 @@
+package jaiden.chapter4.lesson2;
+
+public class Lesson2exe {
+
+	public Lesson2exe() {
+		//Variable Declaration
+		
+		//Whole Number Variables
+		int moneyAmount; //4 bytes
+		byte myAge; //1 byte
+		short hotDogsPerYear; //2 bytes
+		long dreamBank; //8 bytes
+		
+		//Decimal Variables
+		float myGpa; //4 bytes
+		double pie; //8 bytes
+		
+		// Boolean Variable (true or false)
+		boolean answer; //1 byte
+		
+		//Char Variable (single character)
+		char letter; //2 bytes
+		
+		//Giving Variables Values
+		moneyAmount = 2000;
+		hotDogsPerYear = 500;
+		myAge = 17;
+		dreamBank = 999999999999999L;
+		myGpa = 3.65F;
+		pie = 3.141592654;
+		letter = 'A';
+		answer = false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
