9b33947ecaf5c9bc79219da980f85367d2805f25
JLew15 <zombears@icloud.com>
2020-11-04 10:11:10 -0700

    jadskfjkdsajfkjdsakjf

--- tictactoe/src/tictactoe/TicTacToeAi.java
+++ tictactoe/src/tictactoe/TicTacToeAi.java
@@ -11,15 +11,52 @@ public class TicTacToeAi {
 		String X = "X";
 		String O = "O";
 		String EMPTY = " ";
-		String TIE = "TIE";
 		int NUM_SQUARES = 9;
 		String[] board = newBoard(NUM_SQUARES,EMPTY);
+		int move;
 
 		
 		
 		intro();
 		pieces(X,O);
 		int[] legalMoves = legalMoves(board,NUM_SQUARES,EMPTY);
+		String turn = "X";
+		while (true) {
+
+			printBoard(board);
+		if (turn == human) {
+			move = humanMove(board,NUM_SQUARES,EMPTY);
+			board[move] = human;
+		}
+		else {
+			move = humanMove(board,NUM_SQUARES,EMPTY);
+			board[move] = computer;
+		}
+		turn = nextTurn(turn);
+		String win = checkWinner(board,EMPTY);
+		if (win == "X") {
+			printBoard(board);
+			System.out.println("X has won!");
+			break;
+		}
+		else if (win == "O") {
+			printBoard(board);
+			System.out.println("O has won!");
+			break;
+		}
+		else if (win == "DRAW") {
+			printBoard(board);
+			System.out.println("It's a draw!");
+			break;
+		}
+		}
+		String playAgain = getYesNo("Do you want to play again?");
+		if (playAgain.equals("yes")) {
+			new TicTacToeAi();
+		}
+		else {
+			System.out.println("Thanks for playing!");
+		}
 	}
 	
 	public void intro() {
@@ -112,6 +149,76 @@ public class TicTacToeAi {
 			}
 		}
 		return moves;
+	}
+	
+	public int humanMove(String[] board,int NUM_SQUARES, String EMPTY) {
+		int[] moves = legalMoves(board,NUM_SQUARES,EMPTY);
+		int move = -1;
+		while (true) {
+			move = getGoodInt("Where will you move? (0-8) ",0,NUM_SQUARES);
+			for(int x:moves) {
+				if (move == x){
+					return move;
+				}
+			}
+			System.out.println("That square is already taken.");
+		}
+	}
+	
+	public String nextTurn(String turn) {
+		if (turn == "X") {
+			return "O";
+		}
+		else {
+			return "X";
+		}
+		
+	}
+	public String checkWinner(String[] board, String EMPTY) {
+		for(int a = 0; a<8; a++) {
+			String line = null;
+			switch(a) {
+			case 0:
+				line = board[0] + board[1] + board[2];
+				break;
+			case 1:
+				line = board[3] + board[4] + board[5];
+				break;
+			case 2:
+				line = board[6] + board[7] + board[8];
+				break;
+			case 3:
+				line = board[0] + board[3] + board[6];
+				break;
+			case 4:
+				line = board[1] + board[4] + board[7];
+				break;
+			case 5:
+				line = board[2] + board[5] + board[8];
+				break;
+			case 6:
+				line = board[0] + board[4] + board[8];
+				break;
+			case 7:
+				line = board[2] + board[4] + board[6];
+				break;
+			}
+			if (line.equals("XXX")) {
+				return "X";
+			}
+			else if(line.equals("OOO")) {
+				return "O";
+			}
+		}
+		for (int a = 0; a < 9; a++) {
+			if (board[a] == EMPTY) {
+				break;
+			}
+			else if (a == 8) {
+				return "DRAW";
+			}
+		}
+		return null;
 	}
 	
 	public static void main(String[] args) {
