64b0a5df0ffc5e213d05b60bf4f2525c6b898aff
JLew15 <zombears@icloud.com>
2021-01-25 11:17:13 -0700

    jdsalkfjlsadkjf

--- BeerAdvisor/app/src/main/java/com/example/beeradvisor/MainActivity.java
+++ BeerAdvisor/app/src/main/java/com/example/beeradvisor/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.beeradvisor;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick_findDrink(View view){
+        TextView brands = (TextView) findViewById(R.id.brands);
+        brands.setText("You clicked the button.");
     }
 }
