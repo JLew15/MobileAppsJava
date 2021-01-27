c8642bbd4d97f0ec532ae656f57ae565f47e4c1f
JLew15 <zombears@icloud.com>
2021-01-27 10:11:48 -0700

    27271 Wed 01/27/2021 10:11

--- BeerAdvisor/app/src/main/java/com/example/beeradvisor/MainActivity.java
+++ BeerAdvisor/app/src/main/java/com/example/beeradvisor/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.*;
+import java.util.*;
 
 public class MainActivity extends Activity {
+    private DrinkExpert expert = new DrinkExpert();
+    private Random r = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,10 @@ public class MainActivity extends Activity {
 
     public void onClick_findDrink(View view){
         TextView brands = (TextView) findViewById(R.id.brands);
-        brands.setText("You clicked the button.");
+        brands.setText("You pushed the button.");
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String selected = color.getSelectedItem().toString();
+        List<String> drinkList = expert.getBrands(selected);
+        brands.setText("Try this drink. " + drinkList.get(r.nextInt(drinkList.size())));
     }
 }
