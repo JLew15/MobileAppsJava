66c3e3a2e68ea104513034721b0bc8a45d0b228e
JLew15 <zombears@icloud.com>
2021-03-15 10:16:46 -0600

    8987 Mon 03/15/2021 10:16

--- FogtheFrog/app/src/main/java/com/example/fogthefrog/Game.java
+++ FogtheFrog/app/src/main/java/com/example/fogthefrog/Game.java
@@ -2,12 +2,18 @@ package com.example.fogthefrog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Game extends AppCompatActivity implements View.OnClickListener{
 
@@ -28,23 +34,66 @@ public class Game extends AppCompatActivity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
+
+        initButtons();
+        setNewMole();
+        setTimer(difficultyLevel * 1000);
     }
 
     @Override
     public void onClick(View v) {
-
+        if(isComplete){
+            return;
+        }
+        if(v == currentMole){
+            score++;
+            TextView scores = (TextView)findViewById(R.id.scoar);
+            scores.setText("Score : "+ score);
+            setNewMole();
+        }
     }
 
     public void gameOver(){
+        isComplete = true;
+        TextView scores = (TextView)findViewById(R.id.scoar);
+        scores.setText("Game Over! \n Score: " + score);
 
+        Intent gameOverIntent = new Intent(this, GameOver.class);
+        gameOverIntent.putExtra("Score", score);
+        gameOverIntent.putExtra("name", playerName);
+        startActivity(gameOverIntent);
+        finish();
     }
 
     public void setNewMole(){
+        Random gen = new Random();
+
+        int ranItem = gen.nextInt(numMoles);
+
+        int newButtonID = myButtonIDs.get(ranItem);
+        if(currentMole != null){
+            currentMole.setVisibility(View.INVISIBLE);
+        }
+        Button newMole = (Button)findViewById(newButtonID);
+        newMole.setVisibility(View.VISIBLE);
+        currentMole = newMole;
 
     }
 
     public void initButtons(){
+        ViewGroup group = (ViewGroup)findViewById(R.id.layoutgroup);
+        View v;
 
+        for(int i = 0; i < group.getChildCount(); i++){
+            v = group.getChildAt(i);
+            if(v instanceof Button){
+                v.setOnClickListener(this);
+                if(!isComplete){
+                    myButtonIDs.add(v.getId());
+                    v.setVisibility(View.INVISIBLE);
+                }
+            }
+        }
     }
 
     public void setTimer(long time){
--- FogtheFrog/app/src/main/java/com/example/fogthefrog/GameOver.java
+++ FogtheFrog/app/src/main/java/com/example/fogthefrog/GameOver.java
@@ -3,12 +3,22 @@ package com.example.fogthefrog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameOver extends AppCompatActivity {
+    int score;
+    String playername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameover);
+        TextView gameOverMsg = (TextView)findViewById(R.id.message);
+        TextView scoreMsg = (TextView)findViewById(R.id.scoremsg);
+        score = getIntent().getExtras().getInt("Score");
+        playername = getIntent().getExtras().getString("name");
+        scoreMsg.setText("You fogged "+score+" frogs");
+        gameOverMsg.setText("Game Over "+playername);
+
     }
 }
